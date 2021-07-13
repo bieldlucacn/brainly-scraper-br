56de50d350d8b1fa9fd62af748f74c216e94561d
Gabriel <gabrieldlucacnascimento@gmail.com>
2021-07-13 12:40:39 -0300

    testing again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 setup(
   name = 'brainly_scraperbr',        
   packages = ['brainly_scraperbr'],   
-  version = '0.0.2',    
+  version = '0.0.3T',    
   license='MIT',     
   description = 'brainly scraper brasil', 
   author = 'bieldlucacn',                  
