3e52262c96a0263b15023e932440452efa6cdec2
Gabriel <gabrieldlucacnascimento@gmail.com>
2021-07-13 14:05:38 -0300

    updated versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 setup(
   name = 'brainly_scraperbr',        
   packages = ['brainly_scraperbr'],   
-  version = '0.0.12T',    
+  version = '0.1.6',    
   license='MIT',     
   description = 'brainly scraper brasil', 
   author = 'bieldlucacn',                  
